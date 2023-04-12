e12d9a49a221075dc997c63d9e047c2da89591b9
Greg <greg.pounds3@gmail.com>
2023-04-11 23:17:42 -0500

    sign in and up modeled

--- client/dummySlice.js
+++ client/dummySlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 
-export const fetchCampusAsync = createAsyncThunk('fetch/AllDummys', async()=>{
+export const fetchDummyAsync = createAsyncThunk('fetch/AllDummys', async()=>{
   const {data} = await axios.get('http://localhost:3000/api/users')
   return data
 })
@@ -12,7 +12,7 @@ const dummySlice = createSlice({
   initialState:{},
   reducers:{},
   extraReducers: (builder)=>{
-    builder.addCase(fetchDummyAsync.fullfilled, (state,action)=>{
+    builder.addCase(fetchDummyAsync.fulfilled, (state,action)=>{
       return action.payload;
     });
   }
--- client/index.js
+++ client/index.js
@@ -1,12 +1,17 @@
 import './index.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 import { Provider } from 'react-redux';
 import store from './store'
+import { BrowserRouter as Router } from 'react-router-dom';
+import Main from './components/Main';
 
-ReactDOM.render(
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(
   <Provider store={store}>
-   {/*  */}
-  </Provider>,
-  document.getElementById('app')
+    <Router>
+      <Main />
+    </Router>
+  </Provider>
 );
